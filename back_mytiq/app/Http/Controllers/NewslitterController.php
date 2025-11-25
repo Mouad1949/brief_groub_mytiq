f86657befaae89b1d093bbb8a6076409263d3b1a
mouad <mouadlaalam6@gmail.com>
2025-11-25 14:32:52 +0100

    first setp installation

---
+++ back_mytiq/app/Http/Controllers/NewslitterController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Newslitter;
+use App\Http\Requests\StoreNewslitterRequest;
+use App\Http\Requests\UpdateNewslitterRequest;
+
+class NewslitterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreNewslitterRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Newslitter $newslitter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateNewslitterRequest $request, Newslitter $newslitter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Newslitter $newslitter)
+    {
+        //
+    }
+}
