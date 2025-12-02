aff5d45b0730850833b1990e8c4e656efb0236c5
raniaelbadaoui <elbadaouirania0@gmail.com>
2025-12-02 09:05:12 +0100

    update ticket

--- back_mytiq/app/Http/Controllers/NewslitterController.php
+++ back_mytiq/app/Http/Controllers/NewslitterController.php
@@ -5,33 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Newslitter;
 use App\Http\Requests\StoreNewslitterRequest;
 use App\Http\Requests\UpdateNewslitterRequest;
-use Symfony\Component\HttpFoundation\Request;
 
 class NewslitterController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    
-    public function subscribe( StoreNewslitterRequest $request)
-    {
-        $subscription = Newslitter::create([
-            'email' => $request->email,
-            'is_active' => true,
-            'subscribed_at' => now()
-        ]);
-        return response()->json([
-            'success' => true,
-            'message' => 'Inscription à la newsletter réussie',
-            'subscription' => $subscription
-        ], 201);
-    }
-
-    public function unsubscribe(Request $request){
-        
-    }
-    
-    
     public function index()
     {
         //
